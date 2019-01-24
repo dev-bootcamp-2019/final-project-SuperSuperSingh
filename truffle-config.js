c5854584ed8eae1b09ccaa66f889113fabae9bda
SuperSuperSingh <kreaan.singh@gmail.com>
2019-01-24 16:03:06 +0200

    deployed successfully to Rinkeby testnet but failing verification on etherscan. Cannot find ownable and safemath

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "f91f316aecce48928e237ce915ba97a9";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+var infura = "rinkeby.infura.io/v3/f91f316aecce48928e237ce915ba97a9";
+var HDWallet = require("truffle-hdwallet-provider")
+
 module.exports = {
 	networks: {
 		development: {
 		host: "localhost",
 		port: 8545,
 		network_id: "*"
-		}
+		},
+
+		rinkeby: {
+			provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
+			network_id: 4,          // Rinkeby's id
+			gas: 5500000,        
+		},
 	},
+
+	
+  
 	/*compilers: {
 		solc: {
 			version: "0.5.0", // Fetch exact version from solc-bin (default: truffle's version)
