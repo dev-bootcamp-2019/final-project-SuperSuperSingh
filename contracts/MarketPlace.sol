3dc98b799fd8d46de7227b09bbbb7dc1570aa1f8
SuperSuperSingh <kreaan.singh@gmail.com>
2019-01-26 11:16:50 +0200

    included safemath.sol in doplyed contracts, added revert to marketplace contract

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -415,4 +415,15 @@ contract MarketPlace is Ownable {
             storeBalance = storeFront[_storeID].pendingWithdrawal;
             return storeBalance;
     }
+
+    /** @dev Fallback function - Called if other functions don't match call or
+      * sent ether without data
+      * Typically, called when invalid data is sent
+      * Added so ether sent to this contract is reverted if the contract fails
+      * otherwise, the sender's money is transferred to contract
+     */
+      
+    function() external {
+        revert();
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+var SafeMath = artifacts.require("./SafeMath.sol");
 var MarketPlace = artifacts.require("./MarketPlace.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
   deployer.deploy(MarketPlace);
 };
